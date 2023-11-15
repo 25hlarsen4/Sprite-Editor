0004a5a5925a6c370dceb87ee8f1f050e37fe3d9
matthewlam721 <112060411+matthewlam721@users.noreply.github.com>
2023-11-15 14:11:47 -0700

    Review the spritepreview

--- spritepreview.cpp
+++ spritepreview.cpp
@@ -1,62 +1,38 @@
-/**
- * @file spritepreview.h
- * @author teamname: The QT's
- *
- * @brief
- * CS 3505
- * Assignment Name: A8: Sprite Editor Implementation
- *
- * The spritepreview class is reponsible for displaying the preview of the frame animation on the top right corner
- * of the ui.
- *
- * @date 2023-11-14
- */
 #include <QImage>
 #include <QDebug>
 #include "spritepreview.h"
 #include "qpainter.h"
 
-SpritePreview::SpritePreview(QWidget *parent)
-    : QWidget{parent}
-{
+SpritePreview::SpritePreview(QWidget *parent) : QWidget{parent} {
     currFrame = nullptr;
 }
 
 void SpritePreview::paintEvent(QPaintEvent *) {
-
     QPainter painter(this);
     QRect target(0, 0, 130, 130);
 
     if (currFrame) {
         painter.drawImage(target, currFrame->image);
     }
-
 }
 
 void SpritePreview::addFrame(Frame* frame) {
-
-    if(frame){
+    if (frame) {
         currFrame = frame;
         repaint();
     }
-
 }
 
-void SpritePreview::updatePreview(Frame* frame){
-
-    if(frame){
+void SpritePreview::updatePreview(Frame* frame) {
+    if (frame) {
         currFrame = frame;
-    }
-    else {
+    } else {
         currFrame = nullptr;
     }
     repaint();
-
 }
 
-void SpritePreview::loadNewFrames(QList<Frame*> newFrames){
-
+void SpritePreview::loadNewFrames(QList<Frame*> newFrames) {
     frames = newFrames;
-    currFrame = frames[0];
-
+    currFrame = frames.isEmpty() ? nullptr : frames.first();
 }
